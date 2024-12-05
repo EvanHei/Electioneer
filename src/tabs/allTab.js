ee004c6ca4561d6f34892f8f296459527d232ec0
Evan Heidebrink <heidebrinkevan@gmail.com>
2024-12-05 11:39:59 -0800

    Added search bar
    Added search bar on All tab.

--- src/tabs/allTab.js
+++ src/tabs/allTab.js
@@ -6,14 +6,20 @@ const contentContainer = document.getElementById("content");
 export async function allTabClick() {
     activateTab(document.getElementById('allTab'));
 
-    // TODO: add a search bar here
-
+    // load and sort so the ballots with the latest expiration date are at the top
     const ballots = await loadBallots();
-
-    // sort so the ballots with the latest expiration date are at the top
     ballots.sort((a, b) => new Date(b.endTime) - new Date(a.endTime));
 
-    let content = '<div class="scrollable-box"><div class="item-list">';
+    let content = `
+        <!-- Search Bar -->
+        <div class="input-field">
+            <input type="text" id="searchBar" placeholder="Search" />
+        </div>
+
+        <!-- Ballot List -->
+        <div id="ballotList" class="scrollable-box"><div class="item-list">
+    `;
+
     for (const ballot of ballots) {
         
         // add the 'expired' class if the ballot is over
@@ -31,4 +37,38 @@ export async function allTabClick() {
     content += '</div></div>';
 
     contentContainer.innerHTML = content;
+
+    // configure Search bar
+    const searchBar = document.getElementById('searchBar');
+    searchBar.addEventListener('input', () => searchBarInputUpdate(ballots));
+}
+
+function searchBarInputUpdate(ballots) {
+    const searchQuery = document.getElementById('searchBar').value.toLowerCase();
+    const ballotListContainer = document.getElementById('ballotList');
+
+    // filter ballots based on search query
+    const filteredBallots = ballots.filter(ballot =>
+        ballot.name.toLowerCase().includes(searchQuery)
+    );
+
+    // sort so the ballots with the latest expiration date are at the top
+    filteredBallots.sort((a, b) => new Date(b.endTime) - new Date(a.endTime));
+
+    let filteredContent = '<div class="item-list">';
+    for (const ballot of filteredBallots) {
+        const currentTime = new Date();
+        const endDate = new Date(ballot.endTime);
+        const expiredClass = endDate < currentTime ? 'expired' : '';
+
+        filteredContent += `
+            <div class="item ${expiredClass}" data-address="${ballot.address}">
+                <span>${ballot.name}</span>
+                <span class="subscript">Ends ${ballot.endTime}</span>
+            </div>
+        `;
+    }
+
+    filteredContent += '</div>';
+    ballotListContainer.innerHTML = filteredContent;
 }
